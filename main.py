7e7163a0e88506a492658ec8a85852677ecefb87
surprisedPikachu69 <arvind7447@gmail.com>
2023-02-11 21:34:27 +0530

    rainfall

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 from sklearn.metrics import accuracy_score
 import requests
+import datetime
 
 api_key = 'a41cf401d26849f7bf0133021231102'
 
@@ -47,8 +48,28 @@ def getPH(state):
     return 0
 
 def getRainfall(district):
-    return 40
+    seasons = ['Jan-Feb', 'Mar-May', 'Jun-Sep', 'Oct-Dec']
+    month = datetime.datetime.now().month
+    season = ''
+    
+    for i in range(0, len(seasons)):
+        if month in range(i*3+1, i*3+4):
+            season = seasons[i]
+            break
         
+    rainfall_data = pd.read_csv('rainfall/district_rainfall.csv')
+    for i in range(0, len(rainfall_data)):
+        if rainfall_data.iloc[i, 1] == district.upper():
+            return float(rainfall_data.iloc[i, (seasons.index(season)+15)]) / float(rainfall(seasons.index(season)))
+            
+    return 100
+
+def rainfall(i):
+    seasons = ['Jan-Feb', 'Mar-May', 'Jun-Sep', 'Oct-Dec']
+    rain_div = ['2', '3', '4', '3']
+    
+    return rain_div[i]
+
 def getList(l):
     # convert elements of list to float
     for i in range(0, len(l)):
@@ -71,4 +92,4 @@ def main(state, district):
     predict_data = [[N, P, K, temperature, humidity, ph, rainfall]]
     return str(getList(predict_data))
     
-main('Tamil Nadu', 'Ooty') # mothbeans
+print(main('Tamil Nadu', 'Coimbatore')) # mothbeans
